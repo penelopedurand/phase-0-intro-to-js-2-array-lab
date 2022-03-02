51b7a5710a7fa6089dc29a77ba5413ee970b7ad5
Penelope Durand <penelope.durand@mail.goucher.edu>
2022-03-01 22:08:02 -0500

    commit tree cd37defcbc60395ea858b55f0d92bc8ce83ff856

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = [
+    "Milo",
+    "Otis", 
+    "Garfield"
+];
+
+
+//append new value 
+function destructivelyAppendCat (name) {
+     cats.push(name)
+}
+function destructivelyPrependCat (name){
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat (name) {
+    const allCats = [...cats]
+    allCats.push(name)
+    return allCats;
+}
+function prependCat (name) {
+    const allCats = [...cats]
+    allCats.unshift(name)
+    return allCats;
+}
+function removeLastCat (name) {
+    const allCats = [...cats]
+    allCats.pop(name)
+    return allCats;
+}
+function removeFirstCat (name) {
+    const allCats = [...cats]
+    allCats.shift(name)
+    return allCats;
+}
